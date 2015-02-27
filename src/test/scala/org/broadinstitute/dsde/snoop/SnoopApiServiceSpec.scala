e10cd3dd6b8115eb88cf2bd82943bf7f80190461
PeiLin <plin@broadinstitute.org>
2015-02-27 18:12:14 -0500

    revert the port change, travis failure was unrelated

--- src/test/scala/org/broadinstitute/dsde/snoop/SnoopApiServiceSpec.scala
+++ src/test/scala/org/broadinstitute/dsde/snoop/SnoopApiServiceSpec.scala
@@ -1,12 +1,17 @@
 package org.broadinstitute.dsde.snoop
 
+import org.broadinstitute.dsde.snoop.ws.{WorkflowExecutionJsonSupport, ZamboniSubmissionResult}
 import org.specs2.mutable.Specification
+import spray.httpx.SprayJsonSupport
 import spray.testkit.Specs2RouteTest
 import spray.http._
 import StatusCodes._
+import WorkflowExecutionJsonSupport._
+import SprayJsonSupport._
 
 class SnoopApiServiceSpec extends Specification with SnoopApiService with Specs2RouteTest {
   def actorRefFactory = system
+  val submissionResult = ZamboniSubmissionResult("f00ba4", "SUBMITTED")
 
   "Snoop" should {
       "return a greeting for GET requests to the root path" in {
@@ -14,17 +19,15 @@ class SnoopApiServiceSpec extends Specification with SnoopApiService with Specs2
           responseAs[String] must contain("Snoop web service is operational")
         }
       }
-
       "should return 200" in {
-        Post(s"/workflowExecution", HttpEntity(ContentTypes.`application/json`, s"""{
+        Post("/workflowExecution", HttpEntity(ContentTypes.`application/json`, s"""{
          "submissionId": "f00ba4",
           "authToken": "some-token",
           "requestString":  "{\\"key1\\": \\"value1\\"}"
         }""")) ~>
           snoopRoute ~> check {
           status === OK
-          responseAs[String] must contain("f00ba4")
-          responseAs[String] must contain("SUBMITTED")
+          responseAs[ZamboniSubmissionResult] === submissionResult
         }
       }
   }
